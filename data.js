aee7a30c5e532191c4cd2753a39788622b8a19d0
Beck Qin <enginebeck@gmail.com>
2019-04-26 10:58:26 +0800

    update vis config

--- data.js
+++ data.js
@@ -1,9 +1,10 @@
 const data = [
-    {id: 1, content: 'MSBuild', start: '2019-05-6', end: '2019-05-08'},
-    {id: 2, content: 'Apple WWDC', start: '2019-06-03', end: '2019-06-07'},
-    {id: 3, content: 'Google IO', start: '2019-05-08', end: '2019-05-10'},
-    {id: 4, content: 'GMTC', start: '2019-06-20', end: '2019-06-21'},
-    {id: 5, content: 'Salesforce Developer Conference', start: '2019-05-29', end: '2019-05-30'},
-    {id: 6, content: 'Facebook F8', start: '2019-04-30', end: '2019-05-01'},
-    {id: 7, content: 'QCon', start: '2019-05-06', end: '2019-05-08'}
+    {content: 'MSBuild', start: '2019-05-6', end: '2019-05-08', link: 'https://www.microsoft.com/en-us/build'},
+    {content: 'Vue Conf', start: '2019-06-08 00:00:00', end: '2019-06-08 24:00:00', link: 'https://vueconf.cn/'},
+    {content: 'Unite Shangehai 2019', start: '2019-05-11', end: '2019-05-12', link: 'https://connect.unity.com/events/UniteShanghai2019'},
+    {content: 'Apple WWDC', start: '2019-06-03', end: '2019-06-07', link: 'https://developer.apple.com/wwdc19/'},
+    {content: 'Google IO', start: '2019-05-08', end: '2019-05-10', link: 'https://events.google.com/io/'},
+    {content: 'GMTC', start: '2019-06-20', end: '2019-06-21', link: 'https://gmtc2019.geekbang.org/'},
+    {content: 'Facebook F8', start: '2019-04-30', end: '2019-05-01', link: 'https://www.f8.com/'},
+    {content: 'QCon', start: '2019-05-06', end: '2019-05-08', link: 'https://2019.qconbeijing.com/'}
 ];
--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 const container = document.getElementById('visualization');
-const options = {};
+const options = {
+    verticalScroll: true,
+    template: (item, element, data) => `<a href="${data.link}" target="_blank">${data.content}</a>`
+};
+data.forEach((item, index) => item.id = index);
 new vis.Timeline(container, data, options);
