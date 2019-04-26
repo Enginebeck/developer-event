2ef9222331c9eec46b52992530eedcffd7036bb1
Beck Qin <enginebeck@gmail.com>
2019-04-26 09:06:04 +0800

    init project

---
+++ data.js
@@ -0,0 +1,9 @@
+const data = [
+    {id: 1, content: 'MSBuild', start: '2019-05-6', end: '2019-05-08'},
+    {id: 2, content: 'Apple WWDC', start: '2019-06-03', end: '2019-06-07'},
+    {id: 3, content: 'Google IO', start: '2019-05-08', end: '2019-05-10'},
+    {id: 4, content: 'GMTC', start: '2019-06-20', end: '2019-06-21'},
+    {id: 5, content: 'Salesforce Developer Conference', start: '2019-05-29', end: '2019-05-30'},
+    {id: 6, content: 'Facebook F8', start: '2019-04-30', end: '2019-05-01'},
+    {id: 7, content: 'QCon', start: '2019-05-06', end: '2019-05-08'}
+];
---
+++ main.js
@@ -0,0 +1,3 @@
+const container = document.getElementById('visualization');
+const options = {};
+new vis.Timeline(container, data, options);
