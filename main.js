1f0d3c654dfd2fe7e8ac6d1b632fb2a0239edff3
Beck Qin <enginebeck@gmail.com>
2019-04-26 11:05:39 +0800

    update style

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const container = document.getElementById('visualization');
 const options = {
     verticalScroll: true,
-    template: (item, element, data) => `<a href="${data.link}" target="_blank">${data.content}</a>`
+    align: 'center',
+    template: (item, element, data) => `<a href="${data.link}" class="event-link" target="_blank" title="${data.content}">${data.content}</a>`
 };
 data.forEach((item, index) => item.id = index);
 new vis.Timeline(container, data, options);
